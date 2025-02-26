e07210917596f84e744a35fc3d8ece3d1d17b30d
João Victor Sena Salomão <113044408+Jotavin@users.noreply.github.com>
2025-02-26 14:12:03 +0000

    handles database ssl connection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,6 +3,7 @@ import database from "infra/database.js";
 async function status(request, response) {
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
   );
@@ -11,16 +12,15 @@ async function status(request, response) {
   );
 
   const databaseName = process.env.POSTGRES_DB;
-
   const databaseOpenedConnectionsResult = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [databaseName],
   });
-
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
 
   const updatedAt = new Date().toISOString();
+
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
