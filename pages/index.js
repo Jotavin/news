d1557ac9c60d3845ebf3df5f2d3a902ee69c110b
Joao Victor <jv.sena0@gmail.com>
2025-07-20 22:00:13 -0300

    feat: pedrinhos smile

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Initial home page. 🤞❤</h1>;
+  return (
+    <>
+      <h1>Initial home page. 🤞❤</h1>
+      <p>Pedrinho se voce me ama da uma risadinha😘</p>
+    </>);
+
 }
 
 export default Home;
